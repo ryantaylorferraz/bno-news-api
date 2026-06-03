9d1689acbf4b5a69195499c2d432642a55616085
ryantaylorferraz <ryan.bilac2010@hotmail.com>
2026-06-03 17:02:59 -0300

    fix: correcao

--- src/modules/settings/settings.routes.ts
+++ src/modules/settings/settings.routes.ts
@@ -3,6 +3,6 @@ import { authenticate } from '../../middleware/auth';
 import { getSettingsHandler, upsertSettingsHandler } from './settings.controller';
 
 export async function settingRoutes(app: FastifyInstance) {
-  app.get('/',  { preHandler: [authenticate] }, getSettingsHandler);
-  app.put('/',  { preHandler: [authenticate] }, upsertSettingsHandler);
+  app.get('/',                                  getSettingsHandler);
+  app.put('/', { preHandler: [authenticate] },  upsertSettingsHandler);
 }
